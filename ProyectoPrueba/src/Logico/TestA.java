d8bfd44541749e7bfb34d34577f65d150b01872a
Wendily <Wendily@DESKTOP-PQ4O79B>
2019-07-01 11:37:27 -0400

    Agregando el constructor <3

--- ProyectoPrueba/src/Logico/TestA.java
+++ ProyectoPrueba/src/Logico/TestA.java
@@ -2,4 +2,17 @@ package Logico;
 
 public class TestA {
 
+	private int numero;
+	private String hello;
+	
+	
+	public TestA(int numero, String hello) {
+		super();
+		this.numero = numero;
+		this.hello = hello;
+	}
+	
+	
+	
+	
 }
